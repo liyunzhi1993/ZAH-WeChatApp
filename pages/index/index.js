d027b2b32914269cc5f19140d2e023fb7bc4dade
liyunzhi <liyunzhi1993@gmail.com>
2019-11-13 19:11:59 +0800

    初版

--- pages/index/index.js
+++ pages/index/index.js
@@ -3,17 +3,17 @@
 const app = getApp()
 
 Page({
+  navTo(value)
+  {
+    wx.navigateTo({
+      url: value,
+    })
+  },
   data: {
     motto: 'Hello World',
     userInfo: {},
     hasUserInfo: false,
     canIUse: wx.canIUse('button.open-type.getUserInfo')
-  },
-  //事件处理函数
-  bindViewTap: function() {
-    wx.navigateTo({
-      url: '../logs/logs'
-    })
   },
   onLoad: function () {
     if (app.globalData.userInfo) {
